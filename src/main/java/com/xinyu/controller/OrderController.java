12af62722886179995a46a4795ffde315dbbe4b7
kikm <lijieohyeah@163.com>
2020-04-09 17:50:24 +0800

    新增下载

--- src/main/java/com/xinyu/controller/OrderController.java
+++ src/main/java/com/xinyu/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.xinyu.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,106 +23,112 @@ import com.xinyu.model.Order;
 import com.xinyu.model.Unit;
 import com.xinyu.model.User;
 import com.xinyu.service.IOrderService;
+import com.xinyu.util.FileUpDownLoadUtils;
 
 @Controller
 @RequestMapping("/order")
 public class OrderController {
-	
+
 	@Autowired
 	private IOrderService orderService;
-	
-	
+
 	@RequestMapping("/orderList")
-    public ModelAndView getOrderList() {
+	public ModelAndView getOrderList() {
 		ModelAndView mov = new ModelAndView();
 		List<Unit> unitList = orderService.getUnit();
 		List<Device> dList = orderService.getDeviceList();
 		List<User> orderDepathList = orderService.getDepathList();
 		mov.setViewName("orderList");
 		mov.addObject("statusList", OrderStatus.values());
-		mov.addObject("orderDepathList",orderDepathList);
+		mov.addObject("orderDepathList", orderDepathList);
 		mov.addObject("unitList", unitList);
 		mov.addObject("dList", dList);
-        return mov;
-    }
-	
+		return mov;
+	}
+
 	@RequestMapping("/orderTableData")
 	@ResponseBody
-    public Layui getOrderData(PageBean pageBean,String orderNo,String unitName,String status) {
+	public Layui getOrderData(PageBean pageBean, String orderNo, String unitName, String status) {
 		Order order = new Order();
 		Unit u = new Unit();
 		order.setUnit(u);
-		if(StringUtils.isNotEmpty(orderNo)){
+		if (StringUtils.isNotEmpty(orderNo)) {
 			order.setOrderNo(orderNo);
 		}
-		if(StringUtils.isNotEmpty(unitName)){
+		if (StringUtils.isNotEmpty(unitName)) {
 			u.setName(unitName);
 		}
-		if(StringUtils.isNotEmpty(status)){
+		if (StringUtils.isNotEmpty(status)) {
 			order.setStatus(OrderStatus.valueOf(status));
 		}
 		Layui result = orderService.getOrderData(pageBean, order);
-		
-        return result;
-    }
-	
+
+		return result;
+	}
+
 	@RequestMapping("/getNewOrderNo")
 	@ResponseBody
-    public Layui getInitData() {
-		
+	public Layui getInitData() {
+
 		Layui result = orderService.getNewOrderNo();
-		
-        return result;
-    }
-	
+
+		return result;
+	}
+
 	@RequestMapping("/getPartDataByDevice")
 	@ResponseBody
-    public Layui getPartDataByDevice(Integer deviceId) {
-		
+	public Layui getPartDataByDevice(Integer deviceId) {
+
 		Layui result = orderService.getPartDataByDevice(deviceId);
-		
-        return result;
-    }
-	
+
+		return result;
+	}
+
 	@RequestMapping("/saveOrUpdateOrder")
 	@ResponseBody
-    public Layui saveOrUpdateOrder(HttpServletRequest request,Order order,String deleteFile,String deleteOrderPart) {
-		MultipartHttpServletRequest params=((MultipartHttpServletRequest) request);
-		Layui result = orderService.saveOrUpdateOrder(params,order,deleteFile,deleteOrderPart);
-		
-        return result;
-    }
-	
+	public Layui saveOrUpdateOrder(HttpServletRequest request, Order order, String deleteFile, String deleteOrderPart) {
+		MultipartHttpServletRequest params = ((MultipartHttpServletRequest) request);
+		Layui result = orderService.saveOrUpdateOrder(params, order, deleteFile, deleteOrderPart);
+
+		return result;
+	}
+
 	@RequestMapping("/getOrderInfo")
 	@ResponseBody
-    public Order getOrderInfo(Long id) {
+	public Order getOrderInfo(Long id) {
 		Order result = orderService.getOrderById(id);
-		
-        return result;
-    }
-	
+
+		return result;
+	}
+
 	@RequestMapping("/deleteOrder")
 	@ResponseBody
-    public Layui deleteOrder(String id) {
+	public Layui deleteOrder(String id) {
 		Layui result = orderService.deleteOrder(id);
-		
-        return result;
-    }
-	
+
+		return result;
+	}
+
 	@RequestMapping("/depathOrders")
 	@ResponseBody
-    public Layui depathOrder(String ids,String technicianId,@SessionAttribute(Constance.USER_ID) String depathUserId) {
-		Layui result = orderService.depathOrder(ids,technicianId,depathUserId);
-		
-        return result;
-    }
-	
+	public Layui depathOrder(String ids, String technicianId,
+			@SessionAttribute(Constance.USER_ID) String depathUserId) {
+		Layui result = orderService.depathOrder(ids, technicianId, depathUserId);
+
+		return result;
+	}
+
 	@RequestMapping("/finishOrders")
 	@ResponseBody
-    public Layui finishOrder(String ids,String userId) {
-		Layui result = orderService.finishOrder(ids,userId);
-        return result;
-    }
-	
+	public Layui finishOrder(String ids, String userId) {
+		Layui result = orderService.finishOrder(ids, userId);
+		return result;
+	}
+
+	@RequestMapping("/download")
+	public void download(String path, HttpServletResponse response) {
+		
+		FileUpDownLoadUtils.download(path, response);
+	}
 
 }
